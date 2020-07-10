1bd267857097e6d844dd27a0847701862d594d5c
khanhthi.hce <thi.tran@tpssoft.com.vn>
2020-07-10 17:54:42 +0700

    Karros test

--- src/main/java/org/karros/locators/StudentList.java
+++
@@ -1,9 +0,0 @@
-package org.karros.locators;
-
-import org.openqa.selenium.By;
-
-public class StudentList {
-
-	public static By txtEmail = By.name("email");
-	public static By tblRow = By.xpath("//div/div//tr");
-}
